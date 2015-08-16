7ee21107d3005b1387f0209d0ee3354f3289c733
Danubian <stjohnzack@yahoo.com>
2015-08-16 01:13:11 -0700

    Adding path

--- pageServer.js
+++ pageServer.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+var path = require('path');
 
 app.get('/', function (req, res) {
     res.sendFile(getPath('index.html'));
