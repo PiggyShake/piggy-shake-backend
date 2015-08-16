a466d1e6d8585234a83e28c57f182915218f9bd2
Danubian <stjohnzack@yahoo.com>
2015-08-16 01:01:21 -0700

    File sending response

--- pageServer.js
+++ pageServer.js
@@ -3,8 +3,13 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.sendFile(getPath('index.html'));
 });
+
+function getPath(filename)
+{
+    return path.join(__dirname, '../public', filename)
+}
 
 var server = app.listen(80, function () {
     var host = server.address().address;
